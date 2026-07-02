31718f34ce90746eab693b0465b63a97b8f88283
Z <zijianhuang2000@gmail.com>
2026-07-03 05:16:49 +1000

    ok

--- GoogleTranslateXml/translateSvg.ps1
+++ GoogleTranslateXml/translateSvg.ps1
@@ -1,1 +1,1 @@
-./bin/Debug/net10.0/GoogleTranslateXml.exe  /AKF=../../Secrets/GoogleTranslate/apikey.txt /SL=en /TL="zh-hant" /XPaths="//text/svg:tspan" /F=../Tests/TestTranslation/svg/template1.svg /TF=../Tests/TestTranslation/bin/template1.zh-Hant.svg
+./bin/Debug/net10.0/GoogleTranslateXml.exe  /AKF=../../Secrets/GoogleTranslate/apikey.txt /SL=en /TL="zh-hant" /XPaths="//svg:text/svg:tspan" "//svg:text/svg:tspan" /F=../Tests/TestTranslation/svg/template1.svg /TF=../Tests/TestTranslation/bin/template1.zh-Hant.svg
